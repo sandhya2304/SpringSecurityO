068acaae9de6cc45280a51d13ee300c4e2845a28
ram <sandhyasharma0423@gmail.com>
2017-08-23 17:04:31 +0530

    First commit!!

---
+++ src/main/java/com/security/SpringSecurityOracle/SpringSecurityOracleApplication.java
@@ -0,0 +1,12 @@
+package com.security.SpringSecurityOracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOracleApplication.class, args);
+	}
+}
